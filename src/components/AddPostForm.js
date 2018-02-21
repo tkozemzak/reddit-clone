18138d9c0cc9e5425fe38069e0f6ecd6bb65cbb8
John <jaboyak@gmail.com>
2018-02-21 15:17:09 -0700

    updated AddPostForm

--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -1,14 +1,9 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import { Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
 class AddPostForm extends Component {
-  state = {
-    title: '',
-    body: '',
-    author: '',
-    img_url: ''
-  }
   handleSubmit = () => {
+    this.props.addPost()
   }
   render() {
     return (
@@ -17,39 +12,19 @@ class AddPostForm extends Component {
           <Form onSubmit={this.handleSubmit}>
             <FormGroup>
               <Label for="title-field">Title</Label>
-              <Input
-                type="text"
-                name="title"
-                id="title-field"
-                onChange={e => this.setState({ title: e.target.value })}
-              />
+              <Input type="text" name="title" id="title-field" />
             </FormGroup>
             <FormGroup>
               <Label for="body-field">Body</Label>
-              <Input
-                type="text"
-                name="body"
-                id="body-field"
-                onChange={e => this.setState({ body: e.target.value })}
-              />
+              <Input type="text" name="body" id="body-field" />
             </FormGroup>
             <FormGroup>
               <Label for="author-field">Author</Label>
-              <Input
-                type="text"
-                name="author"
-                id="author-field"
-                onChange={e => this.setState({ author: e.target.value })}
-              />
+              <Input type="text" name="author" id="author-field" />
             </FormGroup>
             <FormGroup>
               <Label for="image-field">Image URL</Label>
-              <Input
-                type="text"
-                name="img_url"
-                id="img_url-field"
-                onChange={e => this.setState({ img_url: e.target.value })}
-              />
+              <Input type="text" name="image" id="image-field" />
             </FormGroup>
             <Button type="submit">Submit</Button>
           </Form>
