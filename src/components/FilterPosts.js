440b1014a05165247ca1628645504f218bbce984
John <jaboyak@gmail.com>
2018-03-05 19:42:55 -0700

    removed sort select from FilterPosts.js

--- src/components/FilterPosts.js
+++ src/components/FilterPosts.js
@@ -6,17 +6,9 @@ class FilterPosts extends Component {
     return (
       <Form inline>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label for="filter-field" className="mr-sm-2">Filter:</Label>
+          <Label for="filter-field" className="mr-sm-2">Filter by title:</Label>
           <Input type="text" name="email" id="filter-field" />
         </FormGroup>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label for="examplePassword" className="mr-sm-2">Sort By: </Label>
-           <Input type="select" name="select" id="exampleSelect">
-            <option>Votes</option>
-            <option>Date Added</option>
-          </Input>
-        </FormGroup>
-        <Button type="submit">Submit</Button>
       </Form>
     )
   }
